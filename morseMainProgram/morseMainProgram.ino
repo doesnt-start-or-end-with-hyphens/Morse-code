00972d1c78afa0f27f403ce965291d30b383d5e0
doesnt-start-or-end-with-hyphens <22109@bayfield-high.school.nz>
2026-09-02 18:14:10 +1200

    0.01
    Official versioning start.

--- morseMainProgram/morseMainProgram.ino
+++ morseMainProgram/morseMainProgram.ino
@@ -1,9 +1,62 @@
-void setup() {
-  // put your setup code here, to run once:
+bool eClk = digitalRead(3);
+bool eCt = digitalRead(2);
+bool morse = digitalRead(4);
+String hello = "hi";
 
+void setup() {
+  morseWrite("SOS");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  
+}
 
+int morseWrite(String message) {
+  Serial.begin(9600);
+  
+  Serial.print(morseI.r);
+}
+
+String morseRead(){
+
+}
+
+int pulse(int length){
+  digitalWrite(morse, HIGH);
+  delay(length);
+  digitalWrite(morse, LOW);
+  delay(0.25);
+}
+
+int menu(){
+
+}
+
+void test(){
+  while (true){
+
+  }
+}
+
+int delay(){
+
+}
+
+int lColour(String colour) {
+  if (colour == "green"){
+    digitalWrite(7, HIGH);
+    digitalWrite(8, LOW);
+  }
+  else if (colour == "red"){
+    digitalWrite(7, LOW);
+    digitalWrite(8, HIGH);
+  }
+  else if (colour == "orange"){
+    digitalWrite(7, HIGH);
+    digitalWrite(8, HIGH);
+  }
+  else{
+    digitalWrite(7, LOW);
+    digitalWrite(7, LOW);
+  }
 }
