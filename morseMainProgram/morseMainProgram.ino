8638001495d53420a7bae615b00968329fa2be50
doesnt-start-or-end-with-hyphens <22109@bayfield-high.school.nz>
2026-09-24 23:08:03 +1200

    V0.0.8
    Added color lighting,. Completed both simple and advanced readback games. Added more buzzer sounds including warnings for entering a mode requiring the terminal. Vastly improoved read logic.

--- morseMainProgram/morseMainProgram.ino
+++ morseMainProgram/morseMainProgram.ino
@@ -4,41 +4,38 @@
 const uint8_t totalEntries = sizeof(morseIndex) / sizeof(database);
 
 
-const uint8_t eClk = 2;
-const uint8_t eCt = 3;
+const uint8_t eClk = 3;
+const uint8_t eCt = 2;
 const uint8_t select = 12;
-const uint8_t ledG = 4;
-const uint8_t ledR = 5;
+const uint8_t ledG = 5;
+const uint8_t ledR = 4;
 const uint8_t morseIn = 7;
 const uint8_t speed = A0;
 const uint8_t buzz = 8;
 const float sensitiv = 1.14;
 const uint8_t maxInputMorseSize = 50;
-const uint8_t menuSize = 4;
 const int defaultDelay = 100;
 const bool defaultBeep = true;
-const float dotBoundTuning = 2.3;
-const float gapBoundTuning = 1.5;
+const float dotBoundTuning = 2.5;
+const float gapBoundTuning = 1.3;
 Encoder selector(eClk, eCt);
 
 void setup() {
   Serial.begin(9600);
+  randomSeed(analogRead(A5));
   pinMode(ledR, OUTPUT);
   pinMode(ledG, OUTPUT);
   pinMode(morseIn, INPUT);
   pinMode(eClk, INPUT);
   pinMode(eCt, INPUT);
   pinMode(select, INPUT);
-  Serial.println(digitalRead(select));
   buzzer("startup");
   delay(500);
   menu();
 }
 
 void loop() {
-  //Serial.println(morseRead());
-  morseWrite("Hello");
-  wait(7);
+  
 }
 
 void morseWrite(String message) {
@@ -68,99 +65,104 @@ void morseWrite(String message) {
   }
 }
 
-String morseRead(){
-  Serial.println("Init morseRead");
-  int timeOn[maxInputMorseSize];
-  uint8_t timeOnIndexSize = 0;
-  int timeOff[maxInputMorseSize];
-  uint8_t timeOffIndexSize = 0;
+String morseRead() {
+  const unsigned long debounceTime = 15;
+  const unsigned long minPulseTime = 30;
+  const unsigned long messageTimeout = 2000;
+  const float onClusterRatio = 1.8;
+  const float offClusterRatio = 1.8;
+  unsigned long timeOn[maxInputMorseSize];
+  unsigned long timeOff[maxInputMorseSize];
+  uint8_t timeOnCount = 0;
+  uint8_t timeOffCount = 0;
   bool userTimeOut = false;
-  uint8_t morseOff[maxInputMorseSize];
-  for (uint8_t idx = 0; idx < maxInputMorseSize; idx++) {
-    morseOff[idx] = 1; 
+  while (!digitalRead(morseIn)) {
+    delay(1);
   }
-  while (!digitalRead(morseIn));
-  while (!userTimeOut){
-    unsigned long timeStart = millis();
-    while (digitalRead(morseIn)){
-      tone(buzz, 1000);
+  delay(debounceTime);
+  while (!userTimeOut && timeOnCount < maxInputMorseSize) {
+    unsigned long startTime = millis();
+    tone(buzz, 1000);
+    while (digitalRead(morseIn)) {
+      delay(1);
     }
     noTone(buzz);
-    unsigned long durationOn = millis() - timeStart;
-    if (durationOn > 20) {
-      timeOn[timeOnIndexSize] = durationOn;
-      timeOnIndexSize += 1;
-    } else {
-      continue; 
+    unsigned long onDuration = millis() - startTime;
+    if (onDuration < minPulseTime) {
+      delay(debounceTime);
+      continue;
     }
-    timeStart = millis();
+    timeOn[timeOnCount++] = onDuration;
+    delay(debounceTime);
+    startTime = millis();
     while (!digitalRead(morseIn)) {
-      if ((millis() - timeStart) >= 2000) {
+      if (millis() - startTime >= messageTimeout) {
         userTimeOut = true;
         break;
       }
+      delay(1);
     }
-    if (!userTimeOut){
-      unsigned long durationOff = millis() - timeStart;
-      if (durationOff > 20) {
-        timeOff[timeOffIndexSize] = durationOff;
-        timeOffIndexSize += 1;
-      }
+    if (userTimeOut) {
+      break;
     }
+    unsigned long offDuration = millis() - startTime;
+    if (offDuration >= minPulseTime && timeOffCount < maxInputMorseSize) {
+      timeOff[timeOffCount++] = offDuration;
+    }
+    delay(debounceTime);
   }
-  int minOn = timeOn[0];
-  int maxOn = timeOn[0];
-  for (uint8_t i = 1; i < timeOnIndexSize; i++) {
-    if (timeOn[i] < minOn) minOn = timeOn[i];
-    if (timeOn[i] > maxOn) maxOn = timeOn[i];
+  if (timeOnCount == 0) {
+    return "";
   }
-  float upperDotBound = (minOn + maxOn) / dotBoundTuning;
-  int minOff = timeOff[0];
-  int maxOff = timeOff[0];
-  for (uint8_t i = 1; i < timeOffIndexSize; i++) {
-    if (timeOff[i] < minOff) minOff = timeOff[i];
-    if (timeOff[i] > maxOff) maxOff = timeOff[i];
-  }
-  float upperSmallGapBound = (minOff + maxOff) / gapBoundTuning;
-  bool morseOn[maxInputMorseSize];
-  for (uint8_t i = 0; i < timeOnIndexSize; i++){
-    morseOn[i] = (timeOn[i] >= upperDotBound);
-  }
-  for (uint8_t i = 0; i < timeOffIndexSize; i++){
-    if (timeOff[i] <= upperSmallGapBound){
-      morseOff[i] = 0; 
+  float dashThreshold = findTimingSplit(timeOn, timeOnCount, onClusterRatio);
+  if (dashThreshold == 0) {
+    unsigned long average = 0;
+    for (uint8_t i = 0; i < timeOnCount; i++) {
+      average += timeOn[i];
+    }
+    average /= timeOnCount;
+    if (average < 300) {
+      dashThreshold = 1000;
     }
     else {
-      morseOff[i] = 1; 
+      dashThreshold = 0;
     }
   }
-  if (timeOnIndexSize > 0) {
-    morseOff[timeOnIndexSize - 1] = 1;
-  }
+  float letterGapThreshold = findTimingSplit(timeOff, timeOffCount, offClusterRatio);
   String message = "";
-  uint8_t letterStart = 0;
-  uint8_t lenOfLetter = 0;
   bool checkedLetter[maxInputMorseSize];
-  for (uint8_t i = 0; i < timeOnIndexSize; i++) {
-    checkedLetter[lenOfLetter] = morseOn[i];
+  uint8_t lenOfLetter = 0;
+  for (uint8_t i = 0; i < timeOnCount; i++) {
+    checkedLetter[lenOfLetter] = timeOn[i] >= dashThreshold;
     lenOfLetter++;
-    if (morseOff[i] >= 1) {
-      for (uint8_t j = 0; j < totalEntries; j++){
-        if (morseIndex[j].len == lenOfLetter){
-          bool match = true;
-          for (uint8_t k = 0; k < lenOfLetter; k++){
-            if (morseIndex[j].morse[k] != checkedLetter[k]){
-              match = false;
-              break;
-            }
-          }
-          if (match){
-            message += morseIndex[j].letter;
+    bool endOfLetter = i == timeOnCount - 1;
+    if (!endOfLetter && i < timeOffCount && letterGapThreshold > 0) {
+      if (timeOff[i] >= letterGapThreshold) {
+        endOfLetter = true;
+      }
+    }
+    if (endOfLetter) {
+      bool found = false;
+      for (uint8_t j = 0; j < totalEntries; j++) {
+        if (morseIndex[j].len != lenOfLetter) {
+          continue;
+        }
+        bool match = true;
+        for (uint8_t k = 0; k < lenOfLetter; k++) {
+          if (morseIndex[j].morse[k] != checkedLetter[k]) {
+            match = false;
             break;
           }
         }
+        if (match) {
+          message += morseIndex[j].letter;
+          found = true;
+          break;
+        }
       }
-      letterStart += lenOfLetter; 
+      if (!found) {
+        message += "?";
+      }
       lenOfLetter = 0;
     }
   }
@@ -170,8 +172,37 @@ String morseRead(){
   return message;
 }
 
-
-
+float findTimingSplit(unsigned long values[], uint8_t count, float minRatio) {
+  if (count < 2) return 0;
+  float low = values[0];
+  float high = values[0];
+  for (uint8_t i = 1; i < count; i++) {
+    if (values[i] < low) low = values[i];
+    if (values[i] > high) high = values[i];
+  }
+  if (low == high) return 0;
+  for (uint8_t iteration = 0; iteration < 10; iteration++) {
+    float threshold = (low + high) / 2.0;
+    unsigned long lowTotal = 0;
+    unsigned long highTotal = 0;
+    uint8_t lowCount = 0;
+    uint8_t highCount = 0;
+    for (uint8_t i = 0; i < count; i++) {
+      if (values[i] < threshold) {
+        lowTotal += values[i];
+        lowCount++;
+      } else {
+        highTotal += values[i];
+        highCount++;
+      }
+    }
+    if (lowCount == 0 || highCount == 0) return 0;
+    low = (float)lowTotal / lowCount;
+    high = (float)highTotal / highCount;
+  }
+  if ((high / low) < minRatio) return 0;
+  return (low + high) / 2.0;
+}
 
 void pulse(bool length){
   lColour("orange");
@@ -198,6 +229,28 @@ void wait(uint8_t step){
   }
 }
 
+void simpleReadBack(){
+  uint8_t size = 42;
+  while (true){
+    String qWord = String(morseIndex[random(0, totalEntries)].letter);
+    bool correct = false;
+    while (!correct){
+      morseWrite(qWord);
+      String answer = morseRead();
+      answer.trim();
+      if (answer == qWord){
+        buzzer("correct");
+        delay(3000);
+        correct = true;
+      }
+      else{
+        buzzer("incorrect");
+        delay(3000);
+      }
+    }
+  }
+}
+
 void morseReadBack(){
   int arraySize = 0;
   for (String c : quizWords){arraySize += 1;}
@@ -206,8 +259,35 @@ void morseReadBack(){
     bool correct = false;
     while (!correct){
       morseWrite(qWord);
-      if (morseRead() == qWord){
+      String answer = morseRead();
+      answer.trim();
+      if (answer == qWord){
         buzzer("correct");
+        correct = true;
+        delay(3000);
+      }
+      else{
+        buzzer("incorrect");
+        delay(3000);
+      }
+    }
+  }
+}
+
+void pcReadBack(){
+  int arraySize = 0;
+  for (String c : quizWords){arraySize += 1;}
+  while(true){
+    String qWord = String(morseIndex[random(0, arraySize-1)].letter);
+    bool correct = false;
+    while (!correct){
+      Serial.print("Encode this as morse: ");
+      Serial.println(qWord);
+      String answer = morseRead();
+      answer.trim();
+      if (answer == qWord){
+        buzzer("correct");
+        correct = true;
         delay(3000);
       }
       else{
@@ -219,6 +299,9 @@ void morseReadBack(){
 }
 
 int menu(){
+  uint8_t menuSize;
+  bool confirm = false;
+  menuSize = 5;
   unsigned long lastDebounceTime = 0;
   long selected = 1;
   bool enter = true;
@@ -227,29 +310,57 @@ int menu(){
   while (true){
     if ((millis() - lastDebounceTime) > debounceDelay) {
       long currentClick = selector.read() / 4;
-      if (select){
-        buzzer("select");
-        delay(2000);
+      if (!digitalRead(select)){
         if (selected == 1){
-          test();
+          buzzer("select");
+          delay(2000);
+          testOutput();
         }
         else if (selected == 2){
+          if (confirm){
+            buzzer("select");
+            delay(2000);
+            testInput();
+          }
+          else{
+            confirm = true;
+            buzzer("PC");
+          }
+        }
+        else if (selected == 3){
+          buzzer("select");
+          delay(2000);
+          simpleReadBack();
+        }
+        else if (selected == 4){
+          buzzer("select");
+          delay(2000);
           morseReadBack();
+        }
+        else if (selected == 5){
+          if (confirm){
+            buzzer("select");
+            delay(2000);
+            pcReadBack();
+          }
+          else{
+            confirm = true;
+            buzzer("PC");
+          }
         }
       }
       if (currentClick != oldPosition) {
         if (enter){
           selected = 1;
-          Serial.println(selected);
           enter = false;
         }
         else if (currentClick > oldPosition){
           selected += 1;
-          Serial.println(selected);
+          confirm = false;
         }
         else if (currentClick < oldPosition){
           selected -= 1;
-          Serial.println(selected);
+          confirm = false;
         }
         if (selected < 1){
           selected = menuSize;
@@ -265,7 +376,14 @@ int menu(){
   }
 }
 
-void test(){
+void testInput(){
+  while (true){
+    Serial.println("Enter morse signal now");
+    Serial.println(morseRead());
+  }
+}
+
+void testOutput(){
   while (true){
     morseWrite("Testing");
   }
@@ -296,20 +414,32 @@ void buzzer(String type){
     delay(150);
   }
   else if (type == "correct"){
+    lColour("green");
     tone(buzz, 523);
     delay(200);
     tone(buzz, 784);
     delay(200);
     tone(buzz, 1046);
     delay(200);
+    lColour("");
   }
   else if (type == "incorrect"){
+    lColour("red");
     tone(buzz, 587);
     delay(200);
     tone(buzz, 554);
     delay(200);
     tone(buzz, 523);
     delay(200);
+    lColour("");
+  }
+  else if (type = "PC"){
+    tone(buzz, 349);
+    delay(150);
+    tone(buzz, 698);
+    delay(150);
+    noTone(buzz);
+    delay(150);
   }
   noTone(buzz);
 }
@@ -325,7 +455,7 @@ void lColour(String colour) {
   }
   else if (colour == "orange"){
     digitalWrite(ledG, HIGH);
-    digitalWrite(8, HIGH);
+    digitalWrite(ledR, HIGH);
   }
   else{
     digitalWrite(ledG, LOW);
